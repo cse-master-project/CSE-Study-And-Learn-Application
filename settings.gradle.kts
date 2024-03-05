3fd9e8f3ec0f25b8083287af6f9e313f7528e541
wndudwkd003 <ymail3@naver.com>
2024-03-05 22:06:52 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CSE-Study-And-Learn-Application"
+include(":app")
