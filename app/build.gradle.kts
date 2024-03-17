6d290f1bad4653076c119421aa0a934e674a1639
wndudwkd003 <ymail3@naver.com>
2024-03-17 23:02:58 +0900

    2024-03-17, KJY 설정 UI 생성
    - 설정 유저 정보 수정 프래그먼트
    - 로그아웃, 회원 탈퇴 다이얼로그
    - 위 UI 생성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,6 @@ dependencies {
 
     // ext
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 }
---
+++ app/src/main/java/com/example/cse_study_and_learn_application/ui/setting/EditUserInfoFragment.kt
@@ -0,0 +1,87 @@
+package com.example.cse_study_and_learn_application.ui.setting
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cse_study_and_learn_application.R
+import com.example.cse_study_and_learn_application.databinding.FragmentEditUserInfoBinding
+import com.example.cse_study_and_learn_application.databinding.FragmentSettingBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [EditUserInfoFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class EditUserInfoFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private var _binding: FragmentEditUserInfoBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.apply {
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main)
+            val layoutParams = navHostFragment?.view?.layoutParams as? ViewGroup.MarginLayoutParams
+            layoutParams?.apply {
+                topMargin = 0
+            }
+        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentEditUserInfoBinding.inflate(inflater, container, false)
+
+        binding.ibBackPres.setOnClickListener {
+            requireActivity().onBackPressed()
+        }
+
+        return binding.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment EditUserInfoFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            EditUserInfoFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
