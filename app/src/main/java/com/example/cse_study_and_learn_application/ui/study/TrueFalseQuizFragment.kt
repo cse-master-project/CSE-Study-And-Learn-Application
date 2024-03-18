8246164e515cc0c390fd562f9777904aec5990de
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-03-18 20:01:04 +0900

    true false quiz fragment UI 추가

---
+++ app/src/main/java/com/example/cse_study_and_learn_application/ui/study/TrueFalseQuizFragment.kt
@@ -0,0 +1,31 @@
+package com.example.cse_study_and_learn_application.ui.study
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
+import com.example.cse_study_and_learn_application.R
+import com.example.cse_study_and_learn_application.databinding.FragmentTrueFalseQuizBinding
+
+
+/**
+ * True false quiz fragment
+ *
+ * @constructor Create empty True false quiz fragment
+ * @author JYH
+ * @since 2024-03-18
+ */
+class TrueFalseQuizFragment : Fragment() {
+
+    lateinit var binding: FragmentTrueFalseQuizBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentTrueFalseQuizBinding.inflate(inflater)
+        return binding.root
+    }
+}
