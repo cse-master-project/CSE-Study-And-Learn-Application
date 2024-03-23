ab73e2ec6aae8e2bda05b26ae462e7e05dd4c9fd
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-03-23 17:02:37 +0900

    multiple choice quiz fragment 디자인 변경

--- app/src/main/java/com/example/cse_study_and_learn_application/ui/study/MultipleChoiceQuizFragment.kt
+++ app/src/main/java/com/example/cse_study_and_learn_application/ui/study/MultipleChoiceQuizFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.cse_study_and_learn_application.databinding.FragmentMultipleChoiceQuizBinding
+import com.google.android.material.card.MaterialCardView
 
 /**
  *
@@ -19,11 +20,41 @@ import com.example.cse_study_and_learn_application.databinding.FragmentMultipleC
 class MultipleChoiceQuizFragment : Fragment() {
 
     private lateinit var binding: FragmentMultipleChoiceQuizBinding
+    private val cards: ArrayList<MaterialCardView> = ArrayList()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentMultipleChoiceQuizBinding.inflate(inflater)
+
+
+        cards.add(binding.cvAnswer1)
+        cards.add(binding.cvAnswer2)
+        cards.add(binding.cvAnswer3)
+        cards.add(binding.cvAnswer4)
+
+        cards[0].setOnClickListener {
+            onlyOneCardViewToggle(cards[0])
+        }
+        cards[1].setOnClickListener {
+            onlyOneCardViewToggle(cards[1])
+        }
+        cards[2].setOnClickListener {
+            onlyOneCardViewToggle(cards[2])
+        }
+        cards[3].setOnClickListener {
+            onlyOneCardViewToggle(cards[3])
+        }
+
         return binding.root
+    }
+
+    private fun onlyOneCardViewToggle(cardView:MaterialCardView) {
+        for(i in cards) {
+            if (i.isChecked) {
+                i.toggle()
+            }
+        }
+        cardView.toggle()
     }
 }
