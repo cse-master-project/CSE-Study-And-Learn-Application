84d6b3cf4736369f480b17819e51eaee0d8c354e
ymail <ymail3@naver.com>
2024-10-07 22:58:43 +0900

    2024-10-26 다크모드 강제 라이트 - 다크모드 강제 라이트로 변경

---
+++ app/src/main/java/com/cslu/cse_study_and_learn_application/connector/RetrofitInstance.kt
@@ -0,0 +1,24 @@
+package com.cslu.cse_study_and_learn_application.connector
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "https://cslu.kro.kr/"
+    private const val PORT = "8080" // 8080
+    private const val SERVER_URL = "$BASE_URL:$PORT"
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val quizQueryApi : QuizQueryApi by lazy {
+        retrofit.create(QuizQueryApi::class.java)
+    }
+
+    val userAccountQueryApi : UserAccountApi by lazy {
+        retrofit.create(UserAccountApi::class.java)
+    }
+}
